bedf569aa673897cd5eb9531516a6dfe337f0d3b
vorujack <vorujack@gmail.com>
2022-07-29 18:39:39 +0430

    fix problem of guard script

--- src/main/scala/rosen/bridge/Scripts.scala
+++ src/main/scala/rosen/bridge/Scripts.scala
@@ -363,21 +363,19 @@ object Scripts {
        |  // ----------------- TOKENS
        |  // 0: GuardNFT
        |  // --------------------
-       |  val GuardNFT = fromBase64("GUARD_NFT");
        |  val GuardBox = INPUTS(0);
        |  val signedColl = SELF.R4[Coll[Coll[Byte]]].get.map { (row: Coll[Byte]) => proveDlog(decodePoint(row)) }
        |  val updateSignCount = SELF.R5[Coll[Int]].get(1)
        |  val updateSignLeast = atLeast(updateSignCount, signedColl)
-       |  val verifyGuard = GuardBox.tokens.exists { (token: (Coll[Byte], Long)) => token._1 == GuardNFT };
        |  sigmaProp(
-       |      allOf(
-       |        Coll(
-       |          SELF.propositionBytes == GuardBox.propositionBytes,
-       |          OUTPUTS(0).propositionBytes == SELF.propositionBytes,
-       |          OUTPUTS(0).tokens(0)._1 == SELF.tokens(0)._1,
-       |          updateSignLeast
-       |        )
+       |    allOf(
+       |      Coll(
+       |        SELF.propositionBytes == GuardBox.propositionBytes,
+       |        OUTPUTS(0).propositionBytes == SELF.propositionBytes,
+       |        OUTPUTS(0).tokens(0)._1 == SELF.tokens(0)._1,
+       |        updateSignLeast
        |      )
+       |    )
        |  )
        |}""".stripMargin
 
