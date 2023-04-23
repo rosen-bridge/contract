596eb968c8959ce87eb83a7bd21d9e8fe3678877
vorujack <vorujack@gmail.com>
2023-04-23 16:28:05 +0330

    resolve a bug in contracts

--- src/main/scala/rosen/bridge/Scripts.scala
+++ src/main/scala/rosen/bridge/Scripts.scala
@@ -397,16 +397,17 @@ object Scripts {
        |  // 0: GuardNFT
        |  // --------------------
        |  // [GuardSign (sign atleast update required sign on it)] => [GuardSign]
-       |  val GuardBox = INPUTS(0);
+       |  val guardBoxes = INPUTS.filter { (box: Box) => box.tokens.size > 0 && box.tokens(0)._1 == SELF.tokens(0)._1 }
        |  val signedColl = SELF.R4[Coll[Coll[Byte]]].get.map { (row: Coll[Byte]) => proveDlog(decodePoint(row)) }
        |  val updateSignCount = SELF.R5[Coll[Int]].get(1)
        |  val updateSignLeast = atLeast(updateSignCount, signedColl)
        |  sigmaProp(
        |    allOf(
        |      Coll(
-       |        SELF.propositionBytes == GuardBox.propositionBytes,
+       |        guardBoxes.size == 1,
        |        OUTPUTS(0).propositionBytes == SELF.propositionBytes,
        |        OUTPUTS(0).tokens(0)._1 == SELF.tokens(0)._1,
+       |        OUTPUTS(0).tokens(0)._2 == SELF.tokens(0)._2,
        |        updateSignLeast
        |      )
        |    )
